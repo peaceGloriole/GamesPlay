a25ea1413b467d159c670ef6be0f42f58d402093
peaceGloriole <dmtrpwlw@icloud.com>
2024-07-20 12:33:39 +0300

    fixed server, added game list

--- client/src/components/GameList/GameList.jsx
+++ client/src/components/GameList/GameList.jsx
@@ -1,45 +1,22 @@
 import * as gamesAPI from '../../api/game-api.js';
 import { useEffect, useState } from 'react';
+import GameListItem from './gameListItem/GameListItem.jsx';
 
 export default function GameList() {
 
-    const [games, setGames] = useState([]);
+    const [games, setGames] = useState([]); 
 
     useEffect(() => {
         gamesAPI.getAll()
             .then(result => setGames(result));
-    });
+    }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
 
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-
+            {games.map(game => <GameListItem key={game._id} {...game} />)}
+            
             <h3 className="no-articles">No articles yet</h3>
         </section>
     );
---
+++ client/src/components/GameList/gameListItem/GameListItem.jsx
@@ -0,0 +1,18 @@
+export default function GameListItem({
+    _id,
+    title,
+    category,
+    imageUrl,
+}) {
+    return (
+        <div className="allGames">
+            <div className="allGames-info">
+                <img src={imageUrl}/>
+                <h6>{title}</h6>
+                <h2>{category}</h2>
+                <a href="#" className="details-button">Details</a>
+            </div>
+
+        </div>
+    );
+}
