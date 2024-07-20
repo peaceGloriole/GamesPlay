1f18d259084f85861ad40a1075be664c032b7b2a
peaceGloriole <dmtrpwlw@icloud.com>
2024-07-20 10:51:08 +0300

    main paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
+import CreateGame from "./components/CreateGame/CreateGame";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
 
@@ -11,6 +14,10 @@ function App() {
             <main id="main-content">
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/create" element={<CreateGame />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+
                 </Routes>
             </main>
         </div>
