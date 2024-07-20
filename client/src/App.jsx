79f1c246365faf1d189298190ba62978962b12c5
peaceGloriole <dmtrpwlw@icloud.com>
2024-07-20 09:25:48 +0300

    all html to jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import Catalog from "./components/catalog/Catalog";
+import Create from "./components/create/Create";
+import Details from "./components/details/Details";
+import Edit from "./components/edit/Edit";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
 
@@ -9,6 +15,12 @@ function App() {
 
             <main id="main-content">
                 <Home />
+                <Login />
+                <Register />
+                <Create />
+                <Edit />
+                <Details />
+                <Catalog />
             </main>
         </div>
     );
